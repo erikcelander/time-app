28e1e9ffcce9d78b8ac35e4b44ca4f46f8aac1cf
Erik Kroon Celander <ek223ur@student.lnu.se>
2022-12-11 14:57:23 +0100

    add tracked time model

---
+++ src/model/tracked-time.js
@@ -0,0 +1,12 @@
+
+export class TrackedTime {
+  title
+  duration
+  date
+
+  constructor(title, duration, date) {
+    this.title = title
+    this.duration = duration
+    this.date = date
+  }
+}
